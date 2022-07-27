719d452033507e7e976f531f8d0422211fc6decb
hideto1198 <hideto1198@yahoo.co.jp>
2022-07-27 21:00:11 +0900

    TicketClient追加

---
+++ TheComposableArchitecture/Client/TicketClient.swift
@@ -0,0 +1,43 @@
+//
+//  TicketStore.swift
+//  TheComposableArchitecture
+//
+//  Created by 東　秀斗 on 2022/07/27.
+//
+
+import ComposableArchitecture
+import FirebaseFunctionsCombineSwift
+import FirebaseFunctions
+import Combine
+import Foundation
+
+struct TicketClient {
+    var fetch: () -> Effect<TicketEntity, Failure>
+    struct Failure: Error, Equatable {}
+}
+
+extension TicketClient {
+    static let live = TicketClient(fetch: {
+        Effect.task {
+            let functions = Functions.functions()
+            let data = try await functions.httpsCallable("get_plan").call()
+            var result: TicketEntity = TicketEntity()
+            if let data = data.data {
+                let datas: NSDictionary = (data as! NSDictionary)["data"] as! NSDictionary
+                result = TicketEntity(
+                    name: datas["plan_name"] as! String,
+                    counts: datas["plan_counts"] as! Int,
+                    max_counts: datas["max_plan_counts"] as! Int,
+                    sub_name: datas["sub_plan_name"] as! String,
+                    sub_counts: datas["sub_plan_counts"] as! Int,
+                    sub_max_counts: datas["sub_plan_max_counts"] as! Int
+                )
+                return result
+            }else{
+                return result
+            }
+        }
+        .mapError{ _ in Failure() }
+        .eraseToEffect()
+    })
+}
