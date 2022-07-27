2813f979681c541f2370ea461fe1eea11a26afb7
hideto1198 <hideto1198@yahoo.co.jp>
2022-07-27 20:54:42 +0900

    HomeView追加

---
+++ TheComposableArchitecture/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  TheComposableArchitecture
+//
+//  Created by 東　秀斗 on 2022/07/27.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
