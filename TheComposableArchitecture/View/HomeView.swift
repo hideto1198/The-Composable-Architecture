83051a5ef07f39a24781b5072d66279036ce39ea
hideto1198 <hideto1198@yahoo.co.jp>
2022-07-29 23:14:18 +0900

    MenuView追加

--- TheComposableArchitecture/View/HomeView.swift
+++ TheComposableArchitecture/View/HomeView.swift
@@ -12,9 +12,21 @@ struct HomeView: View {
     let store: Store<HomeState, HomeAction>
     var body: some View {
         WithViewStore(self.store.self){ viewStore in
-            VStack {
-                ReservationView(viewStore: viewStore)
-                TicketView(store: self.store.scope(state: \.ticketState, action: HomeAction.ticketAction))
+            ZStack {
+                VStack {
+                    HomeHeaderView(store: self.store)
+                    ReservationView(viewStore: viewStore)
+                    TicketView(store: self.store.scope(state: \.ticketState, action: HomeAction.ticketAction))
+                }
+                if viewStore.isMenu {
+                    Color.gray
+                        .opacity(0.7)
+                        .edgesIgnoringSafeArea(.vertical)
+                        .onTapGesture {
+                            viewStore.send(.onMenuTap, animation: .easeOut)
+                        }
+                    HomeMenuView()
+                }
             }
         }
     }
