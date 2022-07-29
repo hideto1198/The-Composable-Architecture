0e5fb4d04c46a3ea6e9eecef7d7bbf98ee06a537
hideto1198 <hideto1198@yahoo.co.jp>
2022-07-29 23:12:35 +0900

    HomeHeaderView作成

---
+++ TheComposableArchitecture/View/HomeHeaderView.swift
@@ -0,0 +1,51 @@
+//
+//  HomeHeaderView.swift
+//  TheComposableArchitecture
+//
+//  Created by 東　秀斗 on 2022/07/29.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct HomeHeaderView: View {
+    let store: Store<HomeState, HomeAction>
+    var body: some View {
+        WithViewStore(self.store) { viewStore in
+            VStack {
+                ZStack {
+                    HStack {
+                        Button(
+                            action: {
+                                viewStore.send(.onMenuTap, animation: .easeIn)
+                            }
+                        ){
+                            Image(systemName: "line.horizontal.3")
+                                .resizable()
+                                .frame(width: bounds.width * 0.06, height: bounds.width * 0.06)
+                                .foregroundColor(.primary)
+                        }
+                        Spacer()
+                    }
+                    HStack {
+                        Spacer()
+                        Text("BONA FIDE")
+                        Spacer()
+                    }
+                }
+                .padding(.horizontal)
+                Divider()
+            }
+        }
+    }
+}
+
+struct HomeHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeHeaderView(store:
+                        Store(initialState: HomeState(),
+                              reducer: homeReducer,
+                              environment: .live)
+        )
+    }
+}
