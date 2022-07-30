c7f2f7d14a40715daca53efff36f5ffc65fe5f98
hideto1198 <hideto1198@yahoo.co.jp>
2022-07-31 02:06:29 +0900

    HomeMenuViewの要素になるMenuView作成

---
+++ TheComposableArchitecture/View/MenuView.swift
@@ -0,0 +1,28 @@
+//
+//  MenuView.swift
+//  TheComposableArchitecture
+//
+//  Created by 東　秀斗 on 2022/07/31.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var title: String
+    var body: some View {
+        ZStack(alignment: .leading) {
+            RoundedRectangle(cornerRadius: 5)
+                .stroke()
+            Text("\(title)")
+                .padding(.leading)
+        }
+        .frame(width: bounds.width * 0.6, height: bounds.height * 0.05)
+        .offset(x: bounds.width * 0.3)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView(title: "予約")
+    }
+}
