069f78237689a5a24fb5ead9c2eee443cb44b19f
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-03 08:04:40 +0900

    storeからViewStoreを受け取るように変更

--- TheComposableArchitecture/View/LaunchDesignView.swift
+++ TheComposableArchitecture/View/LaunchDesignView.swift
@@ -9,46 +9,45 @@ import SwiftUI
 import ComposableArchitecture
 
 struct LaunchDesignView: View {
-    // var viewStore: ViewStore<LaunchState, LaunchAction>
-    let store: Store<LaunchState, LifecycleAction<TimerAction>>
+    var viewStore: ViewStore<LaunchState, LaunchAction>
+    //let store: Store<Any, LifecycleAction<TimerAction>>
     
     var body: some View {
-        WithViewStore(self.store) { viewStore in
+        ZStack {
+            Color("app_color")
+                .edgesIgnoringSafeArea(.all)
             ZStack {
-                Color("app_color")
-                    .edgesIgnoringSafeArea(.all)
-                ZStack {
-                    Group {
-                        Image("LOGO")
-                            .resizable()
-                        Path { path in
-                            path.addLines([
-                                CGPoint(x: viewStore.state.lines[0].x, y: bounds.width * 0.2774),
-                                CGPoint(x: bounds.width * 0.353, y: bounds.width * 0.2774)
-                            ])
-                        }
-                        .stroke(Color("app_color"), style: StrokeStyle(lineWidth: bounds.width * 0.026))
-                        Path{ path in
-                            path.addLines([
-                                CGPoint(x: viewStore.state.lines[1].x, y: viewStore.state.lines[1].y),
-                                CGPoint(x: bounds.width * 0.2397, y: bounds.width * 0.1138)
-                            ])
-                        }
-                        .stroke(Color("app_color"), style:StrokeStyle(lineWidth: bounds.width * 0.026))
-                        Path{ path in
-                            path.addLines([
-                                CGPoint(x: viewStore.state.lines[2].x, y: viewStore.state.lines[2].y),
-                                CGPoint(x: bounds.width * 0.1816, y: bounds.width * 0.2512)
-                            ])
-                        }
-                        .stroke(Color("app_color"), style:StrokeStyle(lineWidth: bounds.width * 0.026))
+                Group {
+                    Image("LOGO")
+                        .resizable()
+                    Path { path in
+                        path.addLines([
+                            CGPoint(x: viewStore.state.lines[0].x, y: bounds.width * 0.2774),
+                            CGPoint(x: bounds.width * 0.353, y: bounds.width * 0.2774)
+                        ])
                     }
-                    .frame(width: bounds.width * 0.5, height: bounds.width * 0.5)
+                    .stroke(Color("app_color"), style: StrokeStyle(lineWidth: bounds.width * 0.026))
+                    Path{ path in
+                        path.addLines([
+                            CGPoint(x: viewStore.state.lines[1].x, y: viewStore.state.lines[1].y),
+                            CGPoint(x: bounds.width * 0.2397, y: bounds.width * 0.1138)
+                        ])
+                    }
+                    .stroke(Color("app_color"), style:StrokeStyle(lineWidth: bounds.width * 0.026))
+                    Path{ path in
+                        path.addLines([
+                            CGPoint(x: viewStore.state.lines[2].x, y: viewStore.state.lines[2].y),
+                            CGPoint(x: bounds.width * 0.1816, y: bounds.width * 0.2512)
+                        ])
+                    }
+                    .stroke(Color("app_color"), style:StrokeStyle(lineWidth: bounds.width * 0.026))
                 }
+                .opacity(viewStore.state.opacity)
+                .frame(width: bounds.width * 0.5, height: bounds.width * 0.5)
             }
-            .onAppear{
-                viewStore.send(.onAppear)
-            }
+        }
+        .onAppear{
+            viewStore.send(.timer(.onAppear))
         }
     }
 }
@@ -57,10 +56,10 @@ struct LaunchDesignView: View {
  struct LaunchDesignView_Previews: PreviewProvider {
      static var previews: some View {
          LaunchDesignView(
-             store: Store(initialState: LaunchState(),
+             viewStore: ViewStore(Store(initialState: LaunchState(),
                           reducer: launchReducer,
                           environment: .init(mainQueue: .main)
-                         ).scope(state: {_ in LaunchState()}, action: LaunchAction.timer)
+                         ))
          )
      }
  }
