7d4b5eaa7f17b73605f019bfa16d59a534d13120
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-03 17:47:08 +0900

    メニューに閉じるボタンを追加

--- TheComposableArchitecture/View/HomeMenuView.swift
+++ TheComposableArchitecture/View/HomeMenuView.swift
@@ -6,35 +6,51 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct HomeMenuView: View {
+    let viewStore: ViewStore<HomeState, HomeAction>
     var body: some View {
-        NavigationView {
-            HStack {
-                VStack(alignment: .leading) {
-                    NavigationLink(destination: EmptyView(), label: {
-                        MenuView(title: "予約")
-                    })
-                    .offset(x: bounds.width * 0.3)
-                    Divider()
-                    NavigationLink(destination: EmptyView(), label: {
-                        MenuView(title: "お問い合わせ")
-                    })
-                    .offset(x: bounds.width * 0.3)
-                    Spacer()
+        HStack {
+            VStack(alignment: .leading) {
+                Button(
+                    action: {
+                        viewStore.send(.onMenuTap, animation: .easeOut(duration: 0.5))
+                    }
+                ){
+                    Image(systemName: "xmark")
+                        .resizable()
+                        .frame(width: bounds.width * 0.06, height: bounds.width * 0.06)
+                        .foregroundColor(.primary)
                 }
-                .padding(.leading)
-                .padding(.top, 50)
+                .offset(x: bounds.width * 0.3)
+                NavigationLink(destination: EmptyView(), label: {
+                    MenuView(title: "予約")
+                })
+                .offset(x: bounds.width * 0.3)
+                Divider()
+                NavigationLink(destination: EmptyView(), label: {
+                    MenuView(title: "お問い合わせ")
+                })
+                .offset(x: bounds.width * 0.3)
                 Spacer()
             }
-            .background(Color.white)
-            .edgesIgnoringSafeArea(.vertical)
+            .padding(.leading)
+            .padding(.top, 50)
+            Spacer()
         }
+        .background(Color.white)
+        .edgesIgnoringSafeArea(.vertical)
+        .offset(x: bounds.width * -0.3)
     }
 }
 
 struct HomeMenuView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeMenuView()
+        HomeMenuView(viewStore: ViewStore(
+            Store(initialState: HomeState(),
+                  reducer: homeReducer,
+                  environment: .live)
+        ))
     }
 }
