c8e743f3d9f0a56727d565013cc5d853681f6e0d
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-09 22:49:46 +0900

    TrainerSelectViewの作成

---
+++ TheComposableArchitecture/View/MakeReservation/TrainerSelectView.swift
@@ -0,0 +1,33 @@
+//
+//  TrainerSelectorView.swift
+//  TheComposableArchitecture
+//
+//  Created by 東　秀斗 on 2022/08/09.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct TrainerSelectView: View {
+    let viewStore: ViewStore<MakeReservationState, MakeReservationAction>
+    var body: some View {
+        HStack {
+            Text("トレーナー")
+            Spacer()
+            Button(
+                action: {
+                    viewStore.send(.onTapTrainer)
+                    debugPrint(viewStore.trainerState.trainers)
+                }
+            ){
+                Text("\(viewStore.trainer)")
+            }
+        }
+    }
+}
+
+struct TrainerSelectView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrainerSelectView(viewStore: ViewStore(Store(initialState: MakeReservationState(), reducer: makeReservationReducer, environment: .live)))
+    }
+}
