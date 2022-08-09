1fa0337ce3953309393cf053a753757462d96ad5
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-09 22:50:44 +0900

    TrainersView作成(選択可能なトレーナーをScrollViewで表示する)

---
+++ TheComposableArchitecture/View/MakeReservation/Trainer/TrainersView.swift
@@ -0,0 +1,20 @@
+//
+//  TrainersView.swift
+//  TheComposableArchitecture
+//
+//  Created by 東　秀斗 on 2022/08/09.
+//
+
+import SwiftUI
+
+struct TrainersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TrainersView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrainersView()
+    }
+}
