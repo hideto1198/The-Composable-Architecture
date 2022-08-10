547fd79604afb399765b762deda513a183b00833
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-10 23:51:49 +0900

    ユーザーログインしているか確認するClient作成

---
+++ TheComposableArchitecture/Client/AuthenticationClient.swift
@@ -0,0 +1,29 @@
+//
+//  AuthenticationClient.swift
+//  TheComposableArchitecture
+//
+//  Created by 東　秀斗 on 2022/08/10.
+//
+import ComposableArchitecture
+import Combine
+import FirebaseAuth
+import Foundation
+
+struct AuthenticationClient {
+    var fetch: () -> Effect<Bool, Failure>
+    struct Failure: Error, Equatable {}
+}
+
+extension AuthenticationClient {
+    static let live = AuthenticationClient(fetch: {
+        Effect.task {
+            if let user = Auth.auth().currentUser {
+                return true
+            } else {
+                return false
+            }
+        }
+        .mapError{ _ in Failure() }
+        .eraseToEffect()
+    })
+}
