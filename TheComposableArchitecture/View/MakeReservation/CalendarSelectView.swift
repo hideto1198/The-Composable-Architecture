94d73990c81d87f048ad5865bd8b1ca565a00aaa
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-10 23:52:37 +0900

    日付を選択するView作成

---
+++ TheComposableArchitecture/View/MakeReservation/CalendarSelectView.swift
@@ -0,0 +1,35 @@
+//
+//  CalendarSelectView.swift
+//  TheComposableArchitecture
+//
+//  Created by 東　秀斗 on 2022/08/10.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct CalendarSelectView: View {
+    let viewStore: ViewStore<MakeReservationState, MakeReservationAction>
+    
+    var body: some View {
+        HStack {
+            Text("日付")
+            Spacer()
+            Button(
+                action: {
+                    viewStore.send(.onTapDate, animation: .easeInOut)
+                }
+            ){
+                Text("\(viewStore.reservation_date)")
+            }
+        }
+    }
+}
+
+struct CalendarSelectView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarSelectView(viewStore: ViewStore(Store(initialState: MakeReservationState(),
+                                                      reducer: makeReservationReducer,
+                                                      environment: .live)))
+    }
+}
