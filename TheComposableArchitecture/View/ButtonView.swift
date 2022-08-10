5cee91e92ec24917b9cce099e70f436640549f2e
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-10 23:52:15 +0900

    App全体で使用するボタンを作成

---
+++ TheComposableArchitecture/View/ButtonView.swift
@@ -0,0 +1,32 @@
+//
+//  ButtonView.swift
+//  TheComposableArchitecture
+//
+//  Created by 東　秀斗 on 2022/08/10.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var text: String
+    var body: some View {
+        HStack {
+            Spacer()
+            ZStack {
+                RoundedRectangle(cornerRadius: 3)
+                    .stroke(Color.gray, lineWidth: 1)
+                Text("\(text)")
+                    .foregroundColor(.primary)
+            }
+            .frame(width: bounds.width * 0.8, height: bounds.height * 0.07)
+            .background(Color.white.opacity(0.2))
+            Spacer()
+        }
+    }
+}
+
+struct ButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonView(text: "追加")
+    }
+}
