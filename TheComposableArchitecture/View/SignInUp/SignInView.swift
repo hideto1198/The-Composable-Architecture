7ce47197e788a0ed9980390de6112f32ab856573
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-12 22:47:13 +0900

    サインインボタンの追加

---
+++ TheComposableArchitecture/View/SignInUp/SignInView.swift
@@ -0,0 +1,64 @@
+//
+//  SignInView.swift
+//  TheComposableArchitecture
+//
+//  Created by 東　秀斗 on 2022/08/12.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct SignInView: View {
+    let store: Store<SignInState, SignInAction>
+    
+    var body: some View {
+        WithViewStore(self.store) { viewStore in
+            ZStack {
+                VStack {
+                    Spacer()
+                        .frame(height: bounds.height * 0.1)
+                    Image("LOGO")
+                        .resizable()
+                        .frame(width: bounds.width * 0.4, height: bounds.width * 0.4)
+                    Spacer()
+                        .frame(height: bounds.height * 0.05)
+                    MailInputview(email: viewStore.binding(\.$email))
+                        .padding(.bottom)
+                    PasswordInputView(password: viewStore.binding(\.$password))
+                    Spacer()
+                    Button(
+                        action: {
+                            viewStore.send(.onTapSignIn)
+                        }
+                    ){
+                        ButtonView(text: "サインイン")
+                    }
+                    .padding(.bottom)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color("app_color").edgesIgnoringSafeArea(.all))
+                
+                if viewStore.isLoading {
+                    ActivityIndicator()
+                }
+                NavigationLink(destination: HomeView(store: Store(initialState: HomeState(),
+                                                                  reducer: homeReducer,
+                                                                  environment: .live))
+                    .navigationBarHidden(true),
+                               isActive: viewStore.binding(\.$isHome), label: { Text("") })
+            }
+            .alert(
+                self.store.scope(state: \.alert),
+                dismiss: .alertDismissed
+            )
+        }
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView(store: Store(initialState: SignInState(),
+                               reducer: signInReducer,
+                                environment: SignInEnvironment(signInClient: .live, mainQueue: .main)))
+    }
+}
