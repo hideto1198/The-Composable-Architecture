519ad584b93c58fb2a3da403128fb3a3aa9883f9
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-17 21:36:36 +0900

    基本情報登録時に入力チェックを行うように変更

--- TheComposableArchitecture/Store/RegisterStore.swift
+++ TheComposableArchitecture/Store/RegisterStore.swift
@@ -15,14 +15,26 @@ struct RegisterState: Equatable {
     @BindableState var lastName2: String = ""   // メイ
     @BindableState var sexSelector: Int = 0
     @BindableState var date: Date = Date()
+    @BindableState var isHome: Bool = false
     var isLoading: Bool = false
-    var isHome: Bool = false
+    var alert: AlertState<RegisterAction>?
+    
+    fileprivate mutating func data_validation() -> Bool {
+        if self.firstName1.isEmpty || self.firstName2.isEmpty || self.lastName1.isEmpty || self.lastName2.isEmpty {
+            self.alert = AlertState(title: TextState("エラー"), message: TextState("必須項目が未入力です。"))
+            return false
+        } else {
+            return true
+        }
+    }
 }
 
 enum RegisterAction: BindableAction, Equatable {
     case binding(BindingAction<RegisterState>)
     case onTapRegister
     case registerResponse(Result<Bool, RegisterClient.Failure>)
+    case alertDismissed
+    case navigateToHome
 }
 
 struct RegisterEnvironment {
@@ -40,15 +52,23 @@ let registerReducer: Reducer = Reducer<RegisterState, RegisterAction, RegisterEn
     case .binding:
         return .none
     case .onTapRegister:
+        if !state.data_validation() {
+            return .none
+        }
+        var dateFormatter: DateFormatter {
+            let dateFormatter: DateFormatter = DateFormatter()
+            dateFormatter.calendar = Calendar(identifier: .gregorian)
+            dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "yyyy/MM/dd", options: 0, locale: Locale(identifier: "ja_JP"))
+            return dateFormatter
+        }
         let request: Dictionary<String, Any> = [
-            "BIRTHDAY": state.date,
+            "BIRTHDAY": dateFormatter.string(from: state.date),
             "FIRSTNAME_1": state.firstName1,
             "LASTNAME_1": state.lastName1,
             "FIRSTNAME_2": state.firstName2,
             "LASTNAME_2": state.lastName2,
             "SEX": state.sexSelector == 0 ? "" : state.sexSelector == 1 ? "男" : "女",
             "memo": "",
-            "new_account": true,
             "pairID": "",
             "plan_counts": 0,
             "plan_max_counts": 0,
@@ -59,12 +79,28 @@ let registerReducer: Reducer = Reducer<RegisterState, RegisterAction, RegisterEn
             "token": ""
         ]
         state.isLoading = true
-        return .none
+        return environemnt.registerClient.fetch(request)
+            .receive(on: environemnt.mainQueue)
+            .catchToEffect(RegisterAction.registerResponse)
     case let .registerResponse(.success(response)):
         state.isLoading = false
+        if response {
+            state.alert = AlertState(title: TextState("確認"),
+                                     message: TextState("登録が完了しました"),
+                                     dismissButton: .default(TextState("OK"), action: .send(.navigateToHome)))
+        }
         return .none
     case .registerResponse(.failure):
         state.isLoading = false
+        state.alert = AlertState(title: TextState("エラー"),
+                                 message: TextState("申し訳ございません、エラーが発生しました。時間をおいて再度お試しください。"))
+        return .none
+    case .alertDismissed:
+        state.alert = nil
+        return .none
+    case .navigateToHome:
+        state.alert = nil
+        state.isHome = true
         return .none
     }
 }
