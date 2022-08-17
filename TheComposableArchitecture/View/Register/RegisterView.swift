4593c41b21bae2dada75f7063443f70cf9f758a4
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-17 21:37:02 +0900

    登録Button追加

--- TheComposableArchitecture/View/Register/RegisterView.swift
+++ TheComposableArchitecture/View/Register/RegisterView.swift
@@ -12,32 +12,56 @@ struct RegisterView: View {
     let store: Store<RegisterState, RegisterAction>
     var body: some View {
         WithViewStore(self.store) { viewStore in
-            VStack(alignment: .leading) {
-                AppHeaderView(title: "基本情報登録")
-                Group {
-                    HStack {
-                        InputTextField(title: "姓", text: viewStore.binding(\.$firstName1))
-                            .padding(.leading)
-                        InputTextField(title: "名", text: viewStore.binding(\.$lastName1))
-                            .padding(.trailing)
-                    }
-                    .padding(.bottom)
-                    HStack {
-                        InputTextField(title: "セイ", text: viewStore.binding(\.$firstName2))
-                            .padding(.leading)
-                        InputTextField(title: "メイ", text: viewStore.binding(\.$lastName2))
-                            .padding(.trailing)
-                    }
-                    .padding(.bottom)
-                    SexSelectorView(selector: viewStore.binding(\.$sexSelector))
+            ZStack {
+                VStack(alignment: .leading) {
+                    AppHeaderView(title: "基本情報登録")
+                    Group {
+                        HStack {
+                            InputTextField(title: "姓", text: viewStore.binding(\.$firstName1))
+                                .padding(.leading)
+                            InputTextField(title: "名", text: viewStore.binding(\.$lastName1))
+                                .padding(.trailing)
+                        }
                         .padding(.bottom)
-                    BirthdaySelectorView(date: viewStore.binding(\.$date))
+                        HStack {
+                            InputTextField(title: "セイ", text: viewStore.binding(\.$firstName2))
+                                .padding(.leading)
+                            InputTextField(title: "メイ", text: viewStore.binding(\.$lastName2))
+                                .padding(.trailing)
+                        }
+                        .padding(.bottom)
+                        SexSelectorView(selector: viewStore.binding(\.$sexSelector))
+                            .padding(.bottom)
+                        BirthdaySelectorView(date: viewStore.binding(\.$date))
+                    }
+                    .padding(.top)
+                    Spacer()
+                    Button(
+                        action: {
+                            viewStore.send(.onTapRegister)
+                        }
+                    ){
+                        ButtonView(text: "登録")
+                    }
                 }
-                .padding(.top)
-                Spacer()
+                NavigationLink(
+                    destination: HomeView(store: Store(initialState: HomeState(),
+                                                       reducer: homeReducer,
+                                                       environment: .live))
+                        .navigationBarHidden(true),
+                    isActive: viewStore.binding(\.$isHome),
+                    label: { Text("") }
+                )
+                if viewStore.isLoading {
+                    ActivityIndicator()
+                }
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color("app_color").edgesIgnoringSafeArea(.all))
+            .alert(
+                self.store.scope(state: \.alert),
+                dismiss: .alertDismissed
+            )
         }
     }
 }
