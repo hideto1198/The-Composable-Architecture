73e9fce714aeb6ff9acb45a62d659d5635fad6a9
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-18 23:09:12 +0900

    予約キャンセル後にチケット、予約情報を取得できるように変更

--- TheComposableArchitecture/Store/HomeStore.swift
+++ TheComposableArchitecture/Store/HomeStore.swift
@@ -19,16 +19,20 @@ enum HomeAction: Equatable {
     case ticketAction(TicketAction)
     case onMenuTap
     case onAppear
-    
+    case deleteResponse(Result<Bool, DeleteClient.Failure>)
+    case getTicket
+    case ticketResponse(Result<TicketEntity, TicketClient.Failure>)
 }
 
 struct HomeEnvironment {
     var reservationClient: ReservationClient
     var ticketClient: TicketClient
+    var deleteClient: DeleteClient
     var mainQueue: AnySchedulerOf<DispatchQueue>
     static let live = Self(
         reservationClient: ReservationClient.live,
         ticketClient: TicketClient.live,
+        deleteClient: DeleteClient.live,
         mainQueue: .main
     )
 }
@@ -36,12 +40,19 @@ struct HomeEnvironment {
 let homeReducer: Reducer = Reducer<HomeState, HomeAction, HomeEnvironment>.combine(
     reservationReducer.pullback(state: \HomeState.reservationState,
                                 action: /HomeAction.reservationAction,
-                                environment: { .init(reservationClient: $0.reservationClient, mainQueue: $0.mainQueue)}),
+                                environment: { .init(reservationClient: $0.reservationClient, deleteClient: $0.deleteClient, mainQueue: $0.mainQueue)}),
     ticketReducer.pullback(state: \HomeState.ticketState,
                            action: /HomeAction.ticketAction,
                            environment: { .init(ticketClient: $0.ticketClient, mainQueue: $0.mainQueue)}),
     Reducer { state, action, environment in
         switch action {
+        case let .reservationAction(.onTapDelete(reservation)):
+            state.reservationState.isLoading = true
+            return environment.deleteClient.fetch(reservation)
+                .receive(on: environment.mainQueue)
+                .catchToEffect(HomeAction.deleteResponse)
+        case .ticketAction(.getTicket):
+            return .none
         case .reservationAction, .ticketAction:
             return .none
         case .onMenuTap:
@@ -51,6 +62,24 @@ let homeReducer: Reducer = Reducer<HomeState, HomeAction, HomeEnvironment>.combi
             if UserDefaults.standard.string(forKey: "first_launch") == nil {
                 UserDefaults.standard.set(true, forKey: "first_launch")
             }
+            return .none
+        case let .deleteResponse(.success(response)):
+            state.reservationState.isLoading = false
+            if response {
+                return Effect(value: HomeAction.getTicket)
+            } else {
+                return .none
+            }
+        case .deleteResponse(.failure):
+            return .none
+        case .getTicket:
+            return environment.ticketClient.fetch()
+                .receive(on: environment.mainQueue)
+                .catchToEffect(HomeAction.ticketResponse)
+        case let .ticketResponse(.success(response)):
+            state.ticketState.ticket = response
+            return Effect(value: HomeAction.reservationAction(.getReservation))
+        case .ticketResponse(.failure):
             return .none
         }
     }
