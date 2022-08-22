dfe0ac35eb87f035bdf46b21bb6ed8646e05c89d
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-22 22:44:01 +0900

    基本情報一覧表示するView作成

---
+++ TheComposableArchitecture/View/ProfileView/ProfileView.swift
@@ -0,0 +1,86 @@
+//
+//  ProfileView.swift
+//  TheComposableArchitecture
+//
+//  Created by 東　秀斗 on 2022/08/22.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct ProfileView: View {
+    let store: Store<ProfileState, ProfileAction>
+    var body: some View {
+        WithViewStore(self.store) { viewStore in
+            VStack {
+                AppHeaderView(title: "基本情報")
+                ScrollView {
+                    NavigationLink(
+                        destination: EditUserNameView(store: self.store)
+                            .navigationBarHidden(true),
+                        label: {
+                            ProfileContentView(title: "名前",
+                                               value: UserDefaults.standard.string(forKey: "user_name")!)
+                        }
+                    )
+                    Divider()
+                    NavigationLink(
+                        destination: EditUserNameView(store: self.store)
+                            .navigationBarHidden(true),
+                        label: {
+                            ProfileContentView(title: "カナ",
+                                               value: UserDefaults.standard.string(forKey: "user_kana_name")!)
+                        }
+                    )
+                    Divider()
+                    NavigationLink(
+                        destination: EditUserBirthdayView(store: self.store)
+                            .navigationBarHidden(true),
+                        label: {
+                            ProfileContentView(title: "生年月日",
+                                               value: UserDefaults.standard.string(forKey: "birthday")!)
+                        }
+                    )
+                    Divider()
+                    NavigationLink(
+                        destination: EditUserSexView(store: self.store)
+                            .navigationBarHidden(true),
+                        label: {
+                            ProfileContentView(title: "性別",
+                                               value: UserDefaults.standard.string(forKey: "sex")!)
+                        }
+                    )
+                    Spacer()
+                    Button(
+                        action: {
+                            viewStore.send(.confirmSignout)
+                        }
+                    ){
+                        ButtonView(text: "サインアウト")
+                    }
+                }
+                Spacer()
+            }
+            NavigationLink(
+                destination: LaunchScreenView(store: Store(initialState: LaunchState(),
+                                                           reducer: launchReducer,
+                                                           environment: LaunchEnvironment(mainQueue: .main, authenticationClient: .live)))
+                .navigationBarHidden(true),
+                isActive: viewStore.binding(\.$isLaunch),
+                label: { Text("") }
+            )
+            .alert(
+                self.store.scope(state: \.alert),
+                dismiss: .alertDismissed
+            )
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView(store: Store(initialState: ProfileState(),
+                                 reducer: profileReducer,
+                                 environment: .live))
+    }
+}
