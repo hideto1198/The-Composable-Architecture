c1ea23dda32818f55904a26c4241d8564d97f616
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-22 22:45:58 +0900

    サインアウトを行う用のClient作成

---
+++ TheComposableArchitecture/Client/SignOutClient.swift
@@ -0,0 +1,27 @@
+//
+//  SignOutClient.swift
+//  TheComposableArchitecture
+//
+//  Created by 東　秀斗 on 2022/08/22.
+//
+
+import Foundation
+import ComposableArchitecture
+import FirebaseAuth
+
+struct SignOutClient {
+    var fetch: () -> Effect<Bool, Failure>
+    struct Failure: Error, Equatable {}
+}
+
+extension SignOutClient {
+    static let live = SignOutClient(fetch: {
+        Effect.task {
+            try Auth.auth().signOut()
+            UserDefaults.standard.removeObject(forKey: "first_launch")
+            return true
+        }
+        .mapError{ _ in Failure() }
+        .eraseToEffect()
+    })
+}
