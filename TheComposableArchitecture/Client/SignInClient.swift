7f9b85b55db1f2f7eead2ca1ca3fc0a8294ad351
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-23 20:29:54 +0900

    サインイン時にUserDefaultsに基本情報を設定する曜日変更

--- TheComposableArchitecture/Client/SignInClient.swift
+++ TheComposableArchitecture/Client/SignInClient.swift
@@ -13,27 +13,36 @@ import FirebaseAuth
 import FirebaseFirestore
 
 struct SignInClient {
-    var fetch: (_ email: String, _ password: String) -> Effect<Bool, Failure>
-    struct Failure: Error, Equatable {}
+    var fetch: (_ email: String, _ password: String, _ withApple: Bool) -> Effect<Bool, Failure>
+    struct Failure: Error, Equatable {
+        static func == (lhs: SignInClient.Failure, rhs: SignInClient.Failure) -> Bool {
+            return true
+        }
+        
+        var wrappedValue: Error
+    }
 }
 
 extension SignInClient {
-    static let live = SignInClient(fetch: { email, password in
+    static let live = SignInClient(fetch: { email, password, withApple in
         Effect.task {
-            let data = try await Auth.auth().signIn(withEmail: email, password: password)
-            if let user = data.user {
-                let db = Firestore.firestore()
-                let data = try await db.collection("USERS").document("\(user.uid)").getDocument()
-                if data.exists {
-                    return true
-                } else {
-                    return false
-                }
+            if !withApple {
+                try await Auth.auth().signIn(withEmail: email, password: password)
+            }
+            let db = Firestore.firestore()
+            let userID: String = Auth.auth().currentUser!.uid
+            let response = try await db.collection("USERS").document("\(userID)").getDocument()
+            if response.exists {
+                UserDefaults.standard.set("\(response.get("FIRSTNAME_1")!) \(response.get("LASTNAME_1")!)", forKey: "user_name")
+                UserDefaults.standard.set("\(response.get("FIRSTNAME_2")!) \(response.get("LASTNAME_2")!)", forKey: "user_kana_name")
+                UserDefaults.standard.set("\(response.get("BIRTHDAY")!)", forKey: "birthday")
+                UserDefaults.standard.set("\(response.get("SEX")!)", forKey: "sex")
+                return true
             } else {
                 return false
             }
         }
-        .mapError{ _ in Failure() }
+        .mapError{ error in Failure(wrappedValue: error) }
         .eraseToEffect()
     })
 }
