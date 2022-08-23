c0c0949d885147265bdede1c75629c7840f54ff4
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-23 20:29:09 +0900

    確認メールが正常に送信されない不具合修正

--- TheComposableArchitecture/Client/SendMailClient.swift
+++ TheComposableArchitecture/Client/SendMailClient.swift
@@ -11,28 +11,38 @@ import FirebaseAuth
 
 struct SendMailClient {
     var fetch: (_ email: String, _ password: String) -> Effect<Bool, Failure>
-    struct Failure: Error, Equatable {}
+    struct Failure: Error, Equatable {
+        static func == (lhs: SendMailClient.Failure, rhs: SendMailClient.Failure) -> Bool {
+            return true
+        }
+        
+        var wrappedValue: Error
+    }
 }
 
 extension SendMailClient {
     static let live = SendMailClient(fetch: { email, password in
         Effect.task {
-            let data = try await Auth.auth().signIn(withEmail: email, password: password)
-            if let user = data.user {
-                if !user.isEmailVerified {
-                    try await user.sendEmailVerification()
+            try Auth.auth().signOut()
+            do {
+                let auth = try await Auth.auth().signIn(withEmail: email, password: password)
+                try await auth.user.reload()
+                if !auth.user.isEmailVerified {
+                    do {
+                        try await auth.user.sendEmailVerification()
+                    } catch {
+                        throw Failure(wrappedValue: error)
+                    }
                 } else {
                     return false
                 }
-            } else {
-                let data = try await Auth.auth().createUser(withEmail: email, password: password)
-                if let user = data.user {
-                    try await user.sendEmailVerification()
-                }
+            } catch {
+                let auth = try await Auth.auth().createUser(withEmail: email, password: password)
+                try await auth.user.sendEmailVerification()
             }
             return true
         }
-        .mapError{ _ in Failure() }
+        .mapError{ error in Failure(wrappedValue: error) }
         .eraseToEffect()
     })
 }
