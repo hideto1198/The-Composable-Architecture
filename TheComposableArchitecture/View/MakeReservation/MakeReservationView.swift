8343029e9cf532a2d93c0c0662c782f5c500a70d
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-24 21:22:53 +0900

    背景色を追加

--- TheComposableArchitecture/View/MakeReservation/MakeReservationView.swift
+++ TheComposableArchitecture/View/MakeReservation/MakeReservationView.swift
@@ -13,73 +13,77 @@ struct MakeReservationView: View {
     let store: Store<MakeReservationState,MakeReservationAction>
     var body: some View {
         WithViewStore(self.store) { viewStore in
-            VStack {
-                AppHeaderView(title: "予約画面")
-                ZStack {
-                    ScrollView{
-                        VStack(alignment: .leading) {
-                            Group {
-                                MenuSelectView(viewStore: viewStore)
-                                    .padding(.top)
-                                PlaceSelectView(viewStore: viewStore)
-                                    .onTapGesture {
-                                        viewStore.send(.ticketAction(.getTicket))
+            ZStack {
+                Color("background")
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    AppHeaderView(title: "予約画面")
+                    ZStack {
+                        ScrollView{
+                            VStack(alignment: .leading) {
+                                Group {
+                                    MenuSelectView(viewStore: viewStore)
+                                        .padding(.top)
+                                    PlaceSelectView(viewStore: viewStore)
+                                        .onTapGesture {
+                                            viewStore.send(.ticketAction(.getTicket))
+                                        }
+                                }
+                                .padding(.leading)
+                                if viewStore.showReservationDate {
+                                    CalendarSelectView(viewStore: viewStore)
+                                        .padding([.horizontal, .top])
+                                }
+                                if viewStore.showCalendar {
+                                    CalendarView(viewStore: viewStore)
+                                }
+                                if viewStore.showTrainerSelector {
+                                    TrainerSelectView(viewStore: viewStore)
+                                        .padding([.horizontal, .top])
+                                    if viewStore.showTrainer {
+                                        TrainersView(viewStore: viewStore)
+                                            .padding(.horizontal)
                                     }
-                            }
-                            .padding(.leading)
-                            if viewStore.showReservationDate {
-                                CalendarSelectView(viewStore: viewStore)
-                                    .padding([.horizontal, .top])
-                            }
-                            if viewStore.showCalendar {
-                                CalendarView(viewStore: viewStore)
-                            }
-                            if viewStore.showTrainerSelector {
-                                TrainerSelectView(viewStore: viewStore)
-                                    .padding([.horizontal, .top])
-                                if viewStore.showTrainer {
-                                    TrainersView(viewStore: viewStore)
+                                }
+                                if viewStore.showReservationTime {
+                                    TimeSelectView(viewStore: viewStore)
+                                        .padding([.horizontal, .top])
+                                    
+                                }
+                                if viewStore.showTimeSchedule {
+                                    TimescheduleView(viewStore: viewStore)
                                         .padding(.horizontal)
                                 }
-                            }
-                            if viewStore.showReservationTime {
-                                TimeSelectView(viewStore: viewStore)
-                                    .padding([.horizontal, .top])
-                                
-                            }
-                            if viewStore.showTimeSchedule {
-                                TimescheduleView(viewStore: viewStore)
-                                    .padding(.horizontal)
-                            }
-                            if viewStore.showAddButton {
-                                Button(
-                                    action: {
-                                        viewStore.send(.onTapAddButton, animation: .easeInOut)
+                                if viewStore.showAddButton {
+                                    Button(
+                                        action: {
+                                            viewStore.send(.onTapAddButton, animation: .easeInOut)
+                                        }
+                                    ){
+                                        ButtonView(text: "追加")
                                     }
-                                ){
-                                    ButtonView(text: "追加")
+                                }
+                            }
+                            Spacer()
+                        }
+                        MakeReservationCheckButtonView(viewStore: viewStore)
+                    }
+                    .sheet(isPresented: viewStore.binding(\.$isSheet)) {
+                        MakeReservationListView(store: self.store)
+                    }
+                }
+                .alert(self.store.scope(state: \.alert), dismiss: .alertDismissed)
+                .gesture(
+                    DragGesture(minimumDistance: 5)
+                        .onEnded{ value in
+                            if value.startLocation.x <= bounds.width * 0.09 && value.startLocation.x * 1.1 < value.location.x{
+                                withAnimation(){
+                                    self.presentaionMode.wrappedValue.dismiss()
                                 }
                             }
                         }
-                        Spacer()
-                    }
-                    MakeReservationCheckButtonView(viewStore: viewStore)
-                }
-                .sheet(isPresented: viewStore.binding(\.$isSheet)) {
-                    MakeReservationListView(store: self.store)
-                }
+                )
             }
-            .alert(self.store.scope(state: \.alert), dismiss: .alertDismissed)
-            .gesture(
-                DragGesture(minimumDistance: 5)
-                    .onEnded{ value in
-                        if value.startLocation.x <= bounds.width * 0.09 && value.startLocation.x * 1.1 < value.location.x{
-                            withAnimation(){
-                                self.presentaionMode.wrappedValue.dismiss()
-                            }
-                        }
-                    }
-            )
         }
     }
 }
