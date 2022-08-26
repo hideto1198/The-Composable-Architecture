35a312fc65f4b16a9a6c246b01175ce5cf36d11b
hideto1198 <hideto1198@yahoo.co.jp>
2022-08-26 22:12:48 +0900

    ローワーキャメルに統一

--- TheComposableArchitecture/View/Home/TicketView.swift
+++ TheComposableArchitecture/View/Home/TicketView.swift
@@ -13,7 +13,7 @@ struct TicketView: View {
     var body: some View {
         WithViewStore(self.store) { viewStore in
             VStack(alignment: .leading) {
-                if viewStore.ticket.counts != 0 || viewStore.ticket.sub_counts != 0{
+                if viewStore.ticket.counts != 0 || viewStore.ticket.subCounts != 0{
                     Text("現在所持しているプラン")
                         .font(.custom("", size: 15))
                     Divider()
@@ -21,18 +21,18 @@ struct TicketView: View {
                 Group {
                     if viewStore.ticket.counts != 0 {
                         Text("\(viewStore.ticket.name)")
-                        Text("<\(viewStore.ticket.max_counts)回>")
+                        Text("<\(viewStore.ticket.maxCounts)回>")
                         HStack {
                             Spacer()
                             Text("残り \(viewStore.ticket.counts) 回")
                         }
                     }
-                    if viewStore.ticket.sub_counts != 0 {
-                        Text("\(viewStore.ticket.sub_name)")
-                        Text("<\(viewStore.ticket.sub_max_counts)回>")
+                    if viewStore.ticket.subCounts != 0 {
+                        Text("\(viewStore.ticket.subName)")
+                        Text("<\(viewStore.ticket.subMaxCounts)回>")
                         HStack {
                             Spacer()
-                            Text("残り \(viewStore.ticket.sub_counts) 回")
+                            Text("残り \(viewStore.ticket.subCounts) 回")
                         }
                     }
                 }
@@ -51,10 +51,10 @@ struct TicketView_Previews: PreviewProvider {
         TicketView(store: Store(initialState: TicketState(ticket: TicketEntity(
                                                     name: "トレーニングプラン",
                                                     counts: 10,
-                                                    max_counts: 10,
-                                                    sub_name: "トレーニングプラン",
-                                                    sub_counts: 20,
-                                                    sub_max_counts: 20)),
+                                                    maxCounts: 10,
+                                                    subName: "トレーニングプラン",
+                                                    subCounts: 20,
+                                                    subMaxCounts: 20)),
                                 reducer: ticketReducer,
                                 environment: TicketEnvironment(ticketClient: .live, mainQueue: .main)
         ))
