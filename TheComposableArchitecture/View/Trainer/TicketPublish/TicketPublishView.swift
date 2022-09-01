a3a71a4dedf216d6af2a1cdb7993cdab3d940922
hideto1198 <hideto1198@yahoo.co.jp>
2022-09-01 23:10:12 +0900

    チケットを発行するView作成

--- TheComposableArchitecture/View/Trainer/TicketPublish/TicketPublishView.swift
+++ TheComposableArchitecture/View/Trainer/TicketPublish/TicketPublishView.swift
@@ -6,15 +6,43 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct TicketPublishView: View {
+    let store: Store<TicketPublishState, TicketPublishAction>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WithViewStore(self.store) { viewStore in
+            VStack {
+                AppHeaderView(title: "チケット発行")
+                HStack {
+                    Text("プラン名")
+                    Spacer()
+                    Text("トレーニングプラン")
+                }
+                .padding([.horizontal, .top])
+                CountSelectView(viewStore: viewStore)
+                Spacer()
+                Button(
+                    action: {
+                        viewStore.send(.onTapPublish)
+                    }
+                ) {
+                    ButtonView(text: "発行")
+                }
+                .padding(.bottom)
+            }
+            .sheet(isPresented: viewStore.binding(\.$showQrCode)) {
+                Image(uiImage: UIImage.makeQrCode(text: viewStore.qrData)!)
+            }
+        }
     }
 }
 
 struct TicketPublishView_Previews: PreviewProvider {
     static var previews: some View {
-        TicketPublishView()
+        TicketPublishView(store: Store(initialState: TicketPublishState(),
+                                       reducer: ticketPublishReducer,
+                                       environment: .live))
     }
 }
