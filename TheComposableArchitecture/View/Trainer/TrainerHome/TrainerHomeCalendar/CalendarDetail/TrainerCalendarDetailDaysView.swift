83ee9cdcdf049b8d0efd28af32887518da99d292
hideto1198 <hideto1198@yahoo.co.jp>
2022-09-01 23:10:49 +0900

    1日の詳細画面を横にスワイプして日にちを変えれるView作成

--- TheComposableArchitecture/View/Trainer/TrainerHome/TrainerHomeCalendar/CalendarDetail/TrainerCalendarDetailDaysView.swift
+++ TheComposableArchitecture/View/Trainer/TrainerHome/TrainerHomeCalendar/CalendarDetail/TrainerCalendarDetailDaysView.swift
@@ -12,14 +12,34 @@ struct TrainerCalendarDetailDaysView: View {
     let viewStore: ViewStore<TrainerHomeState, TrainerHomeAction>
     
     var body: some View {
-        TabView(selection: viewStore.binding(\.$dateSelector)) {
-            ForEach(viewStore.trainerCalendarState.gymDates.indices, id: \.self) { i in
-                TrainerCalendarDetailDayView(viewStore: viewStore,
-                                             date: viewStore.trainerCalendarState.gymDates[i])
+        VStack {
+            TabView(selection: viewStore.binding(\.$dateSelector)) {
+                ForEach(viewStore.trainerCalendarState.gymDates.indices, id: \.self) { i in
+                    TrainerCalendarDetailDayView(viewStore: viewStore,
+                                                 date: viewStore.trainerCalendarState.gymDates[i])
+                }
             }
+            .tabViewStyle(PageTabViewStyle())
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .never))
+            .onChange(of: viewStore.dateSelector) { newValue in
+                viewStore.send(.onChangeDate(newValue))
+                viewStore.send(.gymDetailAction(.getGymDetails(["year": "\(String(viewStore.trainerCalendarState.year))",
+                                                                "month": "\(viewStore.trainerCalendarState.month)",
+                                                                "day": viewStore.trainerCalendarState.gymDates[viewStore.dateSelector],
+                                                                "target": viewStore.trainers[viewStore.trainerSelector]])))
+            }
+            Button(
+                action: {
+                    viewStore.send(.onTapCalendarTile("99"), animation: .easeOut)
+                }
+            ){
+                Image(systemName: "xmark.circle")
+                    .resizable()
+                    .frame(width: bounds.width * 0.07, height: bounds.width * 0.07)
+                    .foregroundColor(Color("primary_white"))
+            }
+            .padding(.bottom)
         }
-        .tabViewStyle(PageTabViewStyle())
-        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .never))
     }
 }
 
