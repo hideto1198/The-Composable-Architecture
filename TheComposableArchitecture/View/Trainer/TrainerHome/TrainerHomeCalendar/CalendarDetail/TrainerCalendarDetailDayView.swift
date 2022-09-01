47e9f8e25203bf3ac6a722ca6ac7e529361ecf06
hideto1198 <hideto1198@yahoo.co.jp>
2022-09-01 23:11:20 +0900

    1日の詳細を引っ張って更新できるように変更

--- TheComposableArchitecture/View/Trainer/TrainerHome/TrainerHomeCalendar/CalendarDetail/TrainerCalendarDetailDayView.swift
+++ TheComposableArchitecture/View/Trainer/TrainerHome/TrainerHomeCalendar/CalendarDetail/TrainerCalendarDetailDayView.swift
@@ -20,28 +20,27 @@ struct TrainerCalendarDetailDayView: View {
                 Text("\(String(viewStore.trainerCalendarState.year))年\(viewStore.trainerCalendarState.month)月\(date)日")
                     .bold()
                     .padding(.vertical)
-                List {
-                    ForEach(viewStore.gymDetailState.details) { detail in
-                        TrainerCalendarDetailRowView(detail: detail)
+                if #available(iOS 15.0, *) {
+                    List {
+                        ForEach(viewStore.gymDetailState.details) { detail in
+                            TrainerCalendarDetailRowView(detail: detail)
+                        }
                     }
-                }
-                .listStyle(.plain)
-                .padding(.bottom)
-                Spacer()
-                Button(
-                    action: {
-                        viewStore.send(.onTapCalendarTile("99"), animation: .easeOut)
+                    .listStyle(.plain)
+                    .refreshable {
+                        viewStore.send(.gymDetailAction(.getGymDetails(["year": "\(String(viewStore.trainerCalendarState.year))",
+                                                                        "month": "\(viewStore.trainerCalendarState.month)",
+                                                                        "day": date,
+                                                                        "target": viewStore.trainers[viewStore.trainerSelector]])))
                     }
-                ){
-                    Image(systemName: "xmark.circle")
-                        .foregroundColor(.primary)
+                    .padding(.bottom)
                 }
             }
             if viewStore.gymDetailState.isLoading {
                 ActivityIndicator()
             }
         }
-        .frame(width: bounds.width * 0.9, height: bounds.height * 0.7)
+        .frame(width: bounds.width * 0.9, height: bounds.height * 0.75)
         .onAppear {
             viewStore.send(.gymDetailAction(.getGymDetails(["year": "\(String(viewStore.trainerCalendarState.year))",
                                                             "month": "\(viewStore.trainerCalendarState.month)",
