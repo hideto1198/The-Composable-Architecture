286aade89d78989c73936dfb5cc61ce78556a5a3
hideto1198 <hideto1198@yahoo.co.jp>
2022-09-03 01:32:53 +0900

    日付を選択する用View作成

---
+++ TheComposableArchitecture/View/Trainer/StoreChange/StoreDateSelectView.swift
@@ -0,0 +1,30 @@
+//
+//  StoreDateSelectView.swift
+//  TheComposableArchitecture
+//
+//  Created by 東　秀斗 on 2022/09/03.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct StoreDateSelectView: View {
+    let viewStore: ViewStore<StorePersonalChangeState, StorePersonalChangeAction>
+    var body: some View {
+        HStack {
+            Text("日付選択")
+            DatePicker("", selection: viewStore.binding(\.$dateSelector), displayedComponents: .date)
+                .labelsHidden()
+            Spacer()
+        }
+        .padding([.horizontal, .top])
+    }
+}
+
+struct StoreDateSelectView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoreDateSelectView(viewStore: ViewStore(Store(initialState: StorePersonalChangeState(),
+                                                       reducer: storePersonalChangeReducer,
+                                                       environment: .live)))
+    }
+}
