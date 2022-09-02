37882c94c85a97696725f185838aa34ae3f7739c
hideto1198 <hideto1198@yahoo.co.jp>
2022-09-03 01:32:33 +0900

    View名変更、Viewの詳細作成

---
+++ TheComposableArchitecture/View/Trainer/StoreChange/StoreChangeView.swift
@@ -0,0 +1,46 @@
+//
+//  StoreChangeView.swift
+//  TheComposableArchitecture
+//
+//  Created by 東　秀斗 on 2022/08/25.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct StoreChangeView: View {
+    let store: Store<StoreChangeState, StoreChangeAction>
+    
+    var body: some View {
+        WithViewStore(self.store) { viewStore in
+            VStack {
+                AppHeaderView(title: "店舗変更")
+                Picker(selection: viewStore.binding(\.$viewSelector), label: Text("")) {
+                    Text("デフォルト")
+                        .tag(0)
+                    Text("日別/時間帯別")
+                        .tag(1)
+                }
+                .pickerStyle(.segmented)
+                .labelsHidden()
+                .padding([.top, .horizontal])
+                
+                if viewStore.viewSelector == 0 {
+                    StoreDefaultChangeView(store: self.store.scope(state: \.storeDefaultChangeState, action: StoreChangeAction.storeDefaultChangeAction))
+                } else {
+                    StorePersonalChangeView(store: self.store.scope(state: \.storePersonalChangeState, action: StoreChangeAction.storePersonalChangeAction))
+                }
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+struct StoreChangeView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoreChangeView(store: Store(initialState: StoreChangeState(),
+                                     reducer: storeChangeReducer,
+                                     environment: .live))
+    }
+}
